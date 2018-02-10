2ff4aff24db1a125dfdac490e73e66a0de756c11
hansake <hansake@users.noreply.github.com>
2018-02-10 15:09:38 +0100

    Add files via upload

---
+++ php/currprev30d.php
@@ -0,0 +1,256 @@
+<!DOCTYPE HTML>
+<html>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+}
+
+td, th {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    width: 180px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+ <head>
+  <title>Current last 30 days</title>
+ </head>
+ <body>
+ <H2>Average current per day during last 30 days</H2>
+  <li>
+    <a href="http:/emonitor.php">Back to main page</a>
+  </li>
+  <br>
+
+<?php
+
+    $hregisters = 30;
+    $registertxt206 = "Current L1 in A"; /* Register 206 */
+    $registertxt207 = "Current L2 in A"; /* Register 207 */
+    $registertxt208 = "Current L3 in A"; /* Register 208 */
+
+    $htime = array_fill(0, $hregisters, 0);
+    $hregister206 = array_fill(0, $hregisters, 0);
+    $hregister207 = array_fill(0, $hregisters, 0);
+    $hregister208 = array_fill(0, $hregisters, 0);
+    $hhour = array_fill(0, $hregisters, 0);
+
+    $db = new SQLite3('/var/db/emon.db');
+    if (!$db)
+    {
+        exit($error);
+    }
+
+    $showdate = date_create();
+
+    $indexdate = date_create('today');
+    /*print($indexdate->format('Y-m-d H:i'));*/
+    $db->busyTimeout(5000);
+
+    for ($i = $hregisters - 1; 0 <= $i; $i--)
+    {
+        /*print("index=");
+        print($i);
+        print(":");
+        print($indexdate->format('Y-m-d H:i'));
+        print("-");*/
+        $endstampindex = date_timestamp_get($indexdate);
+        $hhour[$i] = $endstampindex;
+        date_sub($indexdate, date_interval_create_from_date_string('24 hours'));
+        /*print($indexdate->format('Y-m-d H:i'));
+        print(",");*/
+        $startstampindex = date_timestamp_get($indexdate);
+        if ($i == 0)
+        {
+            date_timestamp_set($showdate, $startstampindex);
+            $showhour = $showdate->format('Y-m-d H:i');
+            $startdate = $showhour;
+        }
+        if ($i == ($hregisters - 1))
+        {
+            date_timestamp_set($showdate, $endstampindex);
+            $showhour = $showdate->format('Y-m-d H:i');
+            $enddate = $showhour;
+        }
+        $statement = $db->prepare('SELECT * FROM Eregisters WHERE timestamp BETWEEN :startstamp AND :endstamp'); 
+        if (!$statement)
+        {
+            exit("Cannot prepare SELECT statement.");
+        }
+
+        $statement->bindValue(':startstamp', $startstampindex);
+        $statement->bindValue(':endstamp', $endstampindex);
+        $results = $statement->execute();
+        if (!$results)
+        {
+            exit("Cannot execute SELECT statement.");
+        }
+
+        $curreg206 = 0;
+        $curreg207 = 0;
+        $curreg208 = 0;
+        $numberrows = 0;
+        while ($row = $results->fetchArray())
+        {
+            $numberrows += 1;
+            $htime[$i] = $row[0];
+            $curreg206 += $row[7];
+            $curreg207 += $row[8];
+            $curreg208 += $row[9];
+        }
+        if ($numberrows > 0)
+        {
+            $hregister206[$i] = $curreg206 / $numberrows;
+            $hregister207[$i] = $curreg207 / $numberrows;
+            $hregister208[$i] = $curreg208 / $numberrows;
+        }
+    }
+
+    header("refresh: 30;");
+
+    /*Set parameters for width & height of graph and chart*/
+    $currspacing = 100.0;
+    $graphwidth = $hregisters * 32; /* 24 * 32 */
+    $graphheight = 500;
+    $topmargin =  30;
+    $botmargin = 40;
+    $leftmargin = 20;
+    $rightmargin = 80;
+    $totalheight = $botmargin + $graphheight + $topmargin;
+    $totalwidth = $leftmargin + $graphwidth + $rightmargin;
+    $leftgraph = $leftmargin;
+    $rightgraph = $leftmargin + $graphwidth;
+    $topgraph = $topmargin;
+    $botgraph = $topmargin + $graphheight;
+
+    $xticker = floor($graphwidth / $hregisters);
+
+    $maxcurr206 = max($hregister206);
+    $maxcurr207 = max($hregister207);
+    $maxcurr208 = max($hregister208);
+    $maxcurr = max($maxcurr206, $maxcurr207, $maxcurr208, ($currspacing * 2));
+
+    $topcurr = 0;
+
+    while ($topcurr < $maxcurr)
+    {
+        $topcurr += $currspacing;
+    }
+
+    $currlines = $topcurr / $currspacing;
+    $yspacing = $graphheight / $currlines;
+    $scalefactorcurr = $graphheight / $topcurr;
+
+    echo "\n  <!-- Current graph total size -->\n  ";
+    echo '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="'.$totalwidth.'" height="'.$totalheight.'" >';
+    echo "\n   <!-- Outline of graph -->\n   ";
+    echo '<line x1="'.$leftgraph.'" y1="'.$topgraph.'" x2="'.$rightgraph.'" y2="'.$topgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    echo '<line x1="'.$leftgraph.'" y1="'.$botgraph.'" x2="'.$rightgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    $topgraphup = $topgraph - 15;
+    echo '<line x1="'.$leftgraph.'" y1="'.$topgraphup.'" x2="'.$leftgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    echo '<line x1="'.$rightgraph.'" y1="'.$topgraphup.'" x2="'.$rightgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+
+    echo "\n   <!-- Start and end date and time -->\n   ";
+    $tx = $leftgraph + 5;
+    echo '<text x="'.$tx.'" y="18" font-family="sans-serif" font-size="15px">';
+    echo $startdate;
+    echo '</text>';
+    $tx = $rightgraph - 145;
+    echo '<text x="'.$tx.'" y="18" font-family="sans-serif" font-size="15px">';
+    echo $enddate;
+    echo '</text>';
+
+    echo "\n   <!-- Color and name of graphs -->\n   ";
+    $cy = $totalheight - 10;
+    $ty = $totalheight - 5;
+    echo '<line x1="50" y1="'.$cy.'" x2="100" y2="'.$cy.'" style="stroke:red;stroke-width:4"/>';
+    echo '<text x="110" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $registertxt206;
+    echo '</text>';
+    echo "\n   ";
+    echo '<line x1="250" y1="'.$cy.'" x2="300" y2="'.$cy.'" style="stroke:green;stroke-width:4"/>';
+    echo '<text x="310" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $registertxt207;
+    echo '</text>';
+    echo "\n   ";
+    echo '<line x1="450" y1="'.$cy.'" x2="500" y2="'.$cy.'" style="stroke:blue;stroke-width:4"/>';
+    echo '<text x="510" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $registertxt208;
+    echo '</text>';
+
+    echo "\n   <!-- Horisontal lines -->\n   ";
+    $currline = 0;
+    for ($y = $botgraph; $y > $topgraph; $y -= $yspacing)
+    {
+        if ($y != $botgraph)
+        {
+            echo '<line x1="'.$leftgraph.'" y1="'.$y.'" x2="'.$rightgraph.'" y2="'.$y.'" style="stroke:black;stroke-width:1"/>';
+        }
+        $tx = $rightgraph + 5;
+        $ty = $y + 5;
+        echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px" transform="rotate(0,'.$rightgraph.','.$ty.')">';
+        $currint = (int)$currline;
+        echo sprintf("%01.2f A", $currint / 100);
+        echo '</text>';
+        echo "\n   ";
+        $currline += $currspacing; 
+    }
+
+    $polystringcurr206 = "";
+    $polystringcurr207 = "";
+    $polystringcurr208 = "";
+    echo "<!-- Verical lines -->";
+    for ($j = 0; $j < $hregisters; $j++)
+    {
+        $x = $leftgraph + $j * $xticker;
+        echo "\n   ";
+        echo '<line x1="'.$x.'" y1="'.$topgraph.'" x2="'.$x.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:1"/>';
+        date_timestamp_set($showdate, $hhour[$j]);
+        $showhour = $showdate->format('d');
+        $tx = $x + $xticker - 10;
+        $ty = $botgraph + 18;
+        echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+        echo $showhour;
+        echo '</text>';
+
+        $gx = $x + $xticker;
+
+        $ycurr206 = $botgraph - $hregister206[$j]*$scalefactorcurr;
+        $polystringcurr206 = $polystringcurr206." ".$gx.",".$ycurr206;
+
+        $ycurr207 = $botgraph - $hregister207[$j]*$scalefactorcurr;
+        $polystringcurr207 = $polystringcurr207." ".$gx.",".$ycurr207;
+
+        $ycurr208 = $botgraph - $hregister208[$j]*$scalefactorcurr;
+        $polystringcurr208 = $polystringcurr208." ".$gx.",".$ycurr208;
+
+    }
+    $tx = $leftgraph - 10;
+    $ty = $botgraph + 18;
+    echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $showhour;
+    echo '</text>';
+
+    echo "\n   <!-- Current graph lines -->\n   ";
+    echo '<polyline points="'.$polystringcurr206.'" style="fill:none;stroke:red;stroke-width:4"/>';
+    echo "\n   ";
+    echo '<polyline points="'.$polystringcurr207.'" style="fill:none;stroke:green;stroke-width:4"/>';
+    echo "\n   ";
+    echo '<polyline points="'.$polystringcurr208.'" style="fill:none;stroke:blue;stroke-width:4"/>';
+    echo "\n  ";
+
+    echo '</svg>';
+    echo "\n";
+
+?>
+ </body>
+</html>
+
---
+++ php/emonitor.php
@@ -0,0 +1,217 @@
+<!DOCTYPE HTML>
+<html>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+}
+
+td, th {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    width: 150px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+ <head>
+  <title>Electricity monitor</title>
+ </head>
+ <body>
+
+<?php
+
+    $registertxt200 = "Voltage L1"; /* Register 200, voltage L1 */
+    $registertxt201 = "Voltage L2"; /* Register 201, voltage L2 */
+    $registertxt202 = "Voltage L3"; /* Register 202, voltage L3 */
+
+    $registertxt203 = "Voltage L1-L2"; /* Register 203, voltage L1-L2 */
+    $registertxt204 = "Voltage L2-L3"; /* Register 204, voltage L2-L3 */
+    $registertxt205 = "Voltage L3-L1"; /* Register 205, voltage L3-L1 */
+
+    $registertxt206 = "Current L1"; /* Register 206, current L1 */
+    $registertxt207 = "Current L2"; /* Register 207, current L2 */
+    $registertxt208 = "Current L3"; /* Register 208, current L3 */
+
+    $registertxt209 = "Power L1"; /* Register 209, power L1 */
+    $registertxt210 = "Power L2"; /* Register 210, power L2 */
+    $registertxt211 = "Power L3"; /* Register 211, power L3 */
+
+    $registertxt275 = "Frequency"; /* Register 275 */
+
+    $register200 = 0.0;
+    $register201 = 0.0;
+    $register202 = 0.0;
+
+    $register203 = 0.0;
+    $register204 = 0.0;
+    $register205 = 0.0;
+
+    $register206 = 0.0;
+    $register207 = 0.0;
+    $register208 = 0.0;
+
+    $register209 = 0.0;
+    $register210 = 0.0;
+    $register211 = 0.0;
+
+    $register275 = 0.0;
+
+    $db = new SQLite3('/var/db/emon.db');
+    if (!$db)
+    {
+        exit($error);
+    }
+
+    $db->busyTimeout(5000);
+
+    /* Read last record */
+    $statement = $db->prepare('SELECT * FROM Eregisters WHERE timestamp = (SELECT MAX(timestamp) FROM Eregisters)');
+    if (!$statement)
+    {
+        exit("Cannot prepare SELECT statement.");
+    }
+    $results = $statement->execute();
+    if (!$results)
+    {
+        exit("Cannot execute SELECT statement.");
+    }
+
+    if ($row = $results->fetchArray())
+    {
+        $starttimestamp = $row[0];
+    }
+
+    header("refresh: 40;");
+
+    $nowdate = date_create();
+    $startdate = date_create();
+    date_timestamp_set($startdate, $starttimestamp);
+
+    echo sprintf("<H2>Electricity monitor</H2>\n");
+    echo "<br>Measured at: ";
+    echo ($startdate->format('Y-m-d H:i'));
+    echo ".";
+    $dinterval = date_diff($nowdate, $startdate);
+    $dint = $dinterval->days*86400 + $dinterval->h*3600 + $dinterval->i*60 + $dinterval->s;
+    if ($dint > 60)
+    {
+        echo "<br>Is seems that the data collection has stopped.";
+        echo "<br>Time is now: ";
+        echo ($nowdate->format('Y-m-d H:i'));
+    }
+
+    $register200 = $row[1];
+    $register201 = $row[2];
+    $register202 = $row[3];
+
+    $register203 = $row[4];
+    $register204 = $row[5];
+    $register205 = $row[6];
+
+    $register206 = $row[7];
+    $register207 = $row[8];
+    $register208 = $row[9];
+
+    $register209 = $row[10];
+    $register210 = $row[11];
+    $register211 = $row[12];
+
+    $register275 = $row[13];
+
+    $register600 = $row[14];
+    $register601 = $row[15];
+
+    $currentdivider = $register600 / $register601;
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $registertxt206);
+    echo sprintf("<th>%s</th>", $registertxt207);
+    echo sprintf("<th>%s</th>", $registertxt208);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.2f A</td>", ($register206 / ($currentdivider * 10)));
+    echo sprintf("<td>%01.2f A</td>", ($register207 / ($currentdivider * 10)));
+    echo sprintf("<td>%01.2f A</td>", ($register208 / ($currentdivider * 10)));
+    echo "</tr>";
+    echo "</table>";
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $registertxt200);
+    echo sprintf("<th>%s</th>", $registertxt201);
+    echo sprintf("<th>%s</th>", $registertxt202);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.1f V</td>", ($register200 / 10));
+    echo sprintf("<td>%01.1f V</td>", ($register201 / 10));
+    echo sprintf("<td>%01.1f V</td>", ($register202 / 10));
+    echo "</tr>";
+    echo "</table>";
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $registertxt203);
+    echo sprintf("<th>%s</th>", $registertxt204);
+    echo sprintf("<th>%s</th>", $registertxt205);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.1f V</td>", ($register203 / 10));
+    echo sprintf("<td>%01.1f V</td>", ($register204 / 10));
+    echo sprintf("<td>%01.1f V</td>", ($register205 / 10));
+    echo "</tr>";
+    echo "</table>";
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $registertxt209);
+    echo sprintf("<th>%s</th>", $registertxt210);
+    echo sprintf("<th>%s</th>", $registertxt211);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.0f W</td>", $register209);
+    echo sprintf("<td>%01.0f W</td>", $register210);
+    echo sprintf("<td>%01.0f W</td>", $register211);
+    echo "</tr>";
+    echo "</table>";
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $registertxt275);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.2f Hz</td>", ($register275 / 100));
+    echo "</tr>";
+    echo "</table>";
+
+
+?>
+
+<H4>Graphs</H4>
+<li>
+   <a href="http:/currprev24h.php">Current during last 24 hours</a>
+</li>
+<li>
+   <a href="http:/currprev30d.php">Current during last 30 days</a>
+</li>
+<li>
+   <a href="http:/wattprev24h.php">Watt during last 24 hours</a>
+</li>
+<li>
+   <a href="http:/wattprev30d.php">Watt during last 30 days</a>
+</li>
+<H4>Documentation</H4>
+<li>
+   <a href="http://192.168.1.20/MediaWiki/index.php?title=Energy_monitor_with_Modbus_on_Raspberry_Pi">Electricity monitor on Raspberry Pi</a>
+</li>
+<br>
+<li>
+   <a href="http:/phpliteadmin.php">Database administration</a>
+</li>
+
+ </body>
+</html>
