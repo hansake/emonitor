aecada785c910f9a57688b2ce30cc6991724a0a8
hansake <hansake@users.noreply.github.com>
2018-02-10 15:12:12 +0100

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML>
+<html>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+}
+
+td, th {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    width: 150px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+ <head>
+  <title>Energy monitor</title>
+ </head>
+ <body>
+
+<?php
+    $countertxt1 = "Total kWh"; /* Counter 1 */
+    $countertxt2 = "Heatpump kWh"; /* Counter 2 */
+    $countertxt3 = "Spa kWh"; /* Counter 3 */
+    $countertxt4 = "Other kWh"; /* Counter 1 - (Counter 2 + Counter 3) */
+
+    $totcntr1 = 0.0;
+    $totcntr2 = 0.0;
+    $totcntr3 = 0.0;
+    $totcntr4 = 0.0;
+
+    $db = new SQLite3('/var/db/powermon.db');
+    if (!$db)
+    {
+        exit($error);
+    }
+
+    $db->busyTimeout(5000);
+
+    $statement = $db->prepare('SELECT * FROM Ecounters WHERE 1');
+    if (!$statement)
+    {
+        exit("Cannot prepare SELECT statement.");
+    }
+    $results = $statement->execute();
+    if (!$results)
+    {
+        exit("Cannot execute SELECT statement.");
+    }
+
+    if ($row = $results->fetchArray())
+    {
+        $starttimestamp = $row[0];
+    }
+
+    $statement = $db->prepare('SELECT * FROM Lecounters WHERE 1');
+    if (!$statement)
+    {
+        exit("Cannot prepare SELECT statement.");
+    }
+    $results = $statement->execute();
+    if (!$results)
+    {
+        exit("Cannot execute SELECT statement.");
+    }
+
+    if ($row = $results->fetchArray())
+    {
+        $letimestamp = $row[1];
+        $totcntr1 = $row[2] / 1000;
+        $totcntr2 = $row[3] / 1000;
+        $totcntr3 = $row[4] / 1000;
+        $totcntr4 = $totcntr1 - ($totcntr2 + $totcntr3);
+    }
+
+    header("refresh: 40;");
+
+    $nowdate = date_create();
+    $startdate = date_create();
+    date_timestamp_set($startdate, $starttimestamp);
+
+    $stampdate = date_create();
+    date_timestamp_set($stampdate, $letimestamp);
+
+    echo sprintf("<H2>Energy consumption monitor</H2>\n");
+    echo ($stampdate->format('Y-m-d H:i'));
+    echo "<br>kWh measured since: ";
+    echo ($startdate->format('Y-m-d H:i'));
+    echo " (only total since that date)";
+
+    $dinterval = date_diff($nowdate, $stampdate);
+    $dint = $dinterval->days*86400 + $dinterval->h*3600 + $dinterval->i*60 + $dinterval->s;
+    if ($dint > 60)
+    {
+        echo "<br>Is seems that the data collection has stopped.";
+        echo "<br>Time is now: ";
+        echo ($nowdate->format('Y-m-d H:i'));
+    }
+
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $countertxt1);
+    echo sprintf("<th>%s</th>", $countertxt2);
+    echo sprintf("<th>%s</th>", $countertxt3);
+    echo sprintf("<th>%s</th>", $countertxt4);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.2f</td>", $totcntr1);
+    echo sprintf("<td>%01.2f</td>", $totcntr2);
+    echo sprintf("<td>%01.2f</td>", $totcntr3);
+    echo sprintf("<td>%01.2f</td>", $totcntr4);
+    echo "</tr>";
+    echo "</table>";
+
+?>
+
+<H4>Graphs</H4>
+<li>
+   <a href="http:/powmon2018jan.php">Energy consumption during January 2018</a>
+</li>
+<li>
+   <a href="http:/powmon2017.php">Energy consumption during 2017</a>
+</li>
+<li>
+   <a href="http:/powmonprev30d.php">Energy consumption during last 30 days</a>
+</li>
+<li>
+   <a href="http:/powmonprevday.php">Energy consumption during last day</a>
+</li>
+<li>
+   <a href="http:/powmonprev24h.php">Energy consumption during last 24h</a>
+</li>
+<H4>Documentation</H4>
+<li>
+   <a href="http://192.168.1.20/MediaWiki/index.php?title=Energy_monitor_on_Raspberry_Pi#Energy_monitor_and_web_interface">Energy monitor on Raspberry Pi</a>
+</li>
+<br>
+<li>
+   <a href="http://192.168.1.73/phpliteadmin.php">Database administration</a>
+</li>
+
+ </body>
+</html>
---
+++ php/powmonsept.php
@@ -0,0 +1,284 @@
+<!DOCTYPE HTML>
+<html>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+}
+
+td, th {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    width: 180px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+ <head>
+  <title>Energy during September</title>
+ </head>
+ <body>
+ <H2>Energy consumption during September</H2>
+  <li>
+    <a href="http:/">Back to main page</a>
+  </li>
+
+<?php
+
+    $hcounters = 30;
+    $countertxt1 = "Total kWh"; /* Counter 1 */
+    $countertxt2 = "Heatpump kWh"; /* Counter 2 */
+    $countertxt3 = "Spa kWh"; /* Counter 3 */
+    $countertxt4 = "Other kWh"; /* Counter 1 - (Counter 2 + Counter 3) */
+
+    $hdifftime = array_fill(0, $hcounters, 0);
+    $hdiffcnt1 = array_fill(0, $hcounters, 0);
+    $hdiffcnt2 = array_fill(0, $hcounters, 0);
+    $hdiffcnt3 = array_fill(0, $hcounters, 0);
+    $totcntr1 = 0.0;
+    $totcntr2 = 0.0;
+    $totcntr3 = 0.0;
+    $totcntr4 = 0.0;
+
+    $db = new SQLite3('/var/db/powermon.db');
+    if (!$db)
+    {
+        exit($error);
+    }
+
+    $indexdate = date_create('last day of September 2017');
+    /*print($indexdate->format('Y-m-d H:i'));*/
+
+    $db->busyTimeout(5000);
+
+    $whdiff1 = 0;
+    $whdiff2 = 0;
+    $whdiff3 = 0;
+    $whdifflast1 = 0;
+    $whdifflast2 = 0;
+    $whdifflast3 = 0;
+    $stampdate = date_create();
+    for ($i = $hcounters; 0 <= $i; $i--)
+    {
+        $timestampindex = date_timestamp_get($indexdate);
+        $statement = $db->prepare('SELECT * FROM Ecounters WHERE timestamp > :stamp');
+        if (!$statement)
+        {
+            exit("Cannot prepare SELECT statement.");
+        }
+
+        $statement->bindValue(':stamp', $timestampindex);
+        $results = $statement->execute();
+        if (!$results)
+        {
+            exit("Cannot execute SELECT statement.");
+        }
+
+        if ($row = $results->fetchArray())
+        {
+            $whdiff1 = $whdifflast1 - $row[1];
+            $whdifflast1 = $row[1];
+            $whdiff2 = $whdifflast2 - $row[2];
+            $whdifflast2 = $row[2];
+            $whdiff3 = $whdifflast3 - $row[3];
+            $whdifflast3 = $row[3];
+            if ($i < $hcounters)
+            {
+                $hdifftime[$i] = $lasttimestamp;
+                $hdiffcnt1[$i] = $whdiff1;
+                $totcntr1 += ($whdiff1 / 1000);
+                $hdiffcnt2[$i] = $whdiff2;
+                $totcntr2 += ($whdiff2 / 1000);
+                $hdiffcnt3[$i] = $whdiff3;
+                $totcntr3 += ($whdiff3 / 1000);
+                $totcntr4 = $totcntr1 - ($totcntr2 + $totcntr3);
+            }
+        }
+        date_sub($indexdate, date_interval_create_from_date_string('24 hours'));
+        $lasttimestamp = $row[0];
+    }
+
+    header("refresh: 30;");
+
+    $showdate = date_create();
+    date_timestamp_set($showdate, $hdifftime[1]);
+    date_sub($showdate, date_interval_create_from_date_string('24 hours'));
+    echo sprintf("<H4>Energy consumption from %s and throughout the month</H4>\n", $showdate->format('Y-m-d H:i'));
+    $showhour = $showdate->format('Y-m-d H:i');
+    $startdate = $showhour;
+    date_timestamp_set($showdate, $hdifftime[$hcounters -1]);
+    $showhour = $showdate->format('Y-m-d H:i');
+    $enddate = $showhour;
+
+    $totcntr4 = $totcntr1 - ($totcntr2 + $totcntr3);
+    echo "<table>";
+    echo "<tr>";
+    echo sprintf("<th>%s</th>", $countertxt1);
+    echo sprintf("<th>%s</th>", $countertxt2);
+    echo sprintf("<th>%s</th>", $countertxt3);
+    echo sprintf("<th>%s</th>", $countertxt4);
+    echo  "</tr>";
+    echo  "<tr>";
+    echo sprintf("<td>%01.2f</td>", $totcntr1);
+    echo sprintf("<td>%01.2f</td>", $totcntr2);
+    echo sprintf("<td>%01.2f</td>", $totcntr3);
+    echo sprintf("<td>%01.2f</td>", $totcntr4);
+    echo "</tr>";
+    echo "</table>";
+    echo "<br>";
+
+    $maxwatts1 = max($hdiffcnt1);
+    $maxwatts2 = max($hdiffcnt2);
+    $maxwatts3 = max($hdiffcnt3);
+    $maxwatts = max($maxwatts1, $maxwatts2, $maxwatts3);
+
+    /*Set parameters for width & height of graph and chart*/
+    $wattspacing = 10000;
+    $graphwidth = $hcounters * 26; /* 30 * 26 */
+    $graphheight = 500;
+    $topmargin =  30;
+    $botmargin = 40;
+    $leftmargin = 20;
+    $rightmargin = 80;
+    $totalheight = $botmargin + $graphheight + $topmargin;
+    $totalwidth = $leftmargin + $graphwidth + $rightmargin;
+    $leftgraph = $leftmargin;
+    $rightgraph = $leftmargin + $graphwidth;
+    $topgraph = $topmargin;
+    $botgraph = $topmargin + $graphheight;
+
+    $xticker = floor($graphwidth / count($hdiffcnt1));
+
+    $topwatt = 0;
+    while ($topwatt < $maxwatts)
+    {
+        $topwatt += $wattspacing;
+    }
+    $wattlines = $topwatt / $wattspacing;
+    $yspacing = $graphheight / $wattlines;
+    $scalefactorwatts = $graphheight / $topwatt;
+
+    echo "\n  <!-- Energy graph total size -->\n  ";
+    echo '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="'.$totalwidth.'" height="'.$totalheight.'" >';
+    echo "\n   <!-- Outline of graph -->\n   ";
+    echo '<line x1="'.$leftgraph.'" y1="'.$topgraph.'" x2="'.$rightgraph.'" y2="'.$topgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    echo '<line x1="'.$leftgraph.'" y1="'.$botgraph.'" x2="'.$rightgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    $topgraphup = $topgraph - 15;
+    echo '<line x1="'.$leftgraph.'" y1="'.$topgraphup.'" x2="'.$leftgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+    echo "\n   ";
+    echo '<line x1="'.$rightgraph.'" y1="'.$topgraphup.'" x2="'.$rightgraph.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:2"/>';
+
+    echo "\n   <!-- Start and end date and time -->\n   ";
+    $tx = $leftgraph + 5;
+    echo '<text x="'.$tx.'" y="18" font-family="sans-serif" font-size="15px">';
+    echo $startdate;
+    echo '</text>';
+    $tx = $leftgraph + 350;
+    echo '<text x="'.$tx.'" y="18" font-family="sans-serif" font-size="15px">';
+    echo 'kWh per day';
+    echo '</text>';
+    $tx = $rightgraph - 145;
+    echo '<text x="'.$tx.'" y="18" font-family="sans-serif" font-size="15px">';
+    echo $enddate;
+    echo '</text>';
+
+    echo "\n   <!-- Color and name of graphs -->\n   ";
+    $cy = $totalheight - 10;
+    $ty = $totalheight - 5;
+    echo '<line x1="50" y1="'.$cy.'" x2="100" y2="'.$cy.'" style="stroke:red;stroke-width:4"/>';
+    echo '<text x="110" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $countertxt1;
+    echo '</text>';
+    echo "\n   ";
+    echo '<line x1="250" y1="'.$cy.'" x2="300" y2="'.$cy.'" style="stroke:green;stroke-width:4"/>';
+    echo '<text x="310" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $countertxt2;
+    echo '</text>';
+    echo "\n   ";
+    echo '<line x1="450" y1="'.$cy.'" x2="500" y2="'.$cy.'" style="stroke:blue;stroke-width:4"/>';
+    echo '<text x="510" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $countertxt3;
+    echo '</text>';
+    echo '<line x1="650" y1="'.$cy.'" x2="700" y2="'.$cy.'" style="stroke:orange;stroke-width:4"/>';
+    echo '<text x="710" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo $countertxt4;
+    echo '</text>';
+
+    echo "\n   <!-- Horisontal lines -->\n   ";
+    $wattline = 0;
+    for ($y = $botgraph; $y > $topgraph; $y -= $yspacing)
+    {
+        if ($y != $botgraph)
+        {
+            echo '<line x1="'.$leftgraph.'" y1="'.$y.'" x2="'.$rightgraph.'" y2="'.$y.'" style="stroke:black;stroke-width:1"/>';
+        }
+        $tx = $rightgraph + 5;
+        $ty = $y + 5;
+        echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px" transform="rotate(0,'.$rightgraph.','.$ty.')">';
+        $wattint = (int)$wattline;
+        echo sprintf("%d kWh", $wattint / 1000);
+        echo '</text>';
+        echo "\n   ";
+        $wattline += $wattspacing; 
+    }
+
+    $polystringwatts1 = "";
+    $polystringwatts2 = "";
+    $polystringwatts3 = "";
+    $polystringwatts4 = "";
+    echo "<!-- Verical lines -->";
+    for ($j = 0; $j < count($hdiffcnt1); $j++)
+    {
+        $x = $leftgraph + $j * $xticker;
+        echo "\n   ";
+        echo '<line x1="'.$x.'" y1="'.$topgraph.'" x2="'.$x.'" y2="'.$botgraph.'" style="stroke:black;stroke-width:1"/>';
+        date_timestamp_set($showdate, $hdifftime[$j]);
+        $showhour = $showdate->format('d');
+        $tx = $x + $xticker - 10;
+        $ty = $botgraph + 18;
+        echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+        echo $showhour;
+        echo '</text>';
+
+        $gx = $x + $xticker;
+
+        $ywatts1 = $botgraph - $hdiffcnt1[$j]*$scalefactorwatts;
+        $polystringwatts1 = $polystringwatts1." ".$gx.",".$ywatts1;
+
+        $ywatts2 = $botgraph - $hdiffcnt2[$j]*$scalefactorwatts;
+        $polystringwatts2 = $polystringwatts2." ".$gx.",".$ywatts2;
+
+        $ywatts3 = $botgraph - $hdiffcnt3[$j]*$scalefactorwatts;
+        $polystringwatts3 = $polystringwatts3." ".$gx.",".$ywatts3;
+
+        $ywatts4 = $botgraph - (($hdiffcnt1[$j] - ($hdiffcnt2[$j] + $hdiffcnt3[$j]))*$scalefactorwatts);
+        $polystringwatts4 = $polystringwatts4." ".$gx.",".$ywatts4;
+
+    }
+    $tx = $leftgraph - 10;
+    $ty = $botgraph + 18;
+    echo '<text x="'.$tx.'" y="'.$ty.'" font-family="sans-serif" font-size="15px">';
+    echo ".";
+    echo '</text>';
+
+    echo "\n   <!-- Energy graph lines -->\n   ";
+    echo '<polyline points="'.$polystringwatts4.'" style="fill:none;stroke:orange;stroke-width:4"/>';
+    echo "\n  ";
+    echo '<polyline points="'.$polystringwatts1.'" style="fill:none;stroke:red;stroke-width:4"/>';
+    echo "\n   ";
+    echo '<polyline points="'.$polystringwatts2.'" style="fill:none;stroke:green;stroke-width:4"/>';
+    echo "\n   ";
+    echo '<polyline points="'.$polystringwatts3.'" style="fill:none;stroke:blue;stroke-width:4"/>';
+    echo "\n  ";
+
+    echo '</svg>';
+    echo "\n";
+
+?>
+ </body>
+</html>
