b07b5dad278d9d832ad4beec9685e8b0f2cead33
Hans-Ake <hansake@users.noreply.github.com>
2018-05-30 17:15:28 +0200

    Add files via upload
    Added graphs for May 2018.

--- php/index.php
+++ php/index.php
@@ -119,6 +119,9 @@ tr:nth-child(even) {
 
 <H4>Graphs</H4>
 <li>
+   <a href="http:/powmon2018may.php">Energy consumption during May 2018</a>
+</li>
+<li>
    <a href="http:/powmon2018apr.php">Energy consumption during April 2018</a>
 </li>
 <li>
