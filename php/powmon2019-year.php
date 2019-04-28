2e1830659a1b4ab66e209b641615eb737ca10c19
Hans-Ake <hansake@users.noreply.github.com>
2019-04-28 19:23:40 +0200

    Add files via upload

---
+++ php/powmon2019-year.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+<?php
+
+$YEAR = '2019';
+
+include 'powmon_year.php';
+
+?>
+
+<H4>Monthly graphs 2019</H4>
+<li>
+   <a href="powmon2019-01.php">Energy consumption during January 2018</a>
+</li>
+<li>
+   <a href="powmon2019-02.php">Energy consumption during February 2018</a>
+</li>
+<li>
+   <a href="powmon2019-03.php">Energy consumption during March 2018</a>
+</li>
+<li>
+   <a href="powmon2019-04.php">Energy consumption during April 2018</a>
+</li>
+<li>
+   <a href="powmon2019-05.php">Energy consumption during May 2018</a>
+</li>
+<li>
+   <a href="powmon2019-06.php">Energy consumption during June 2018</a>
+</li>
+<li>
+   <a href="powmon2019-07.php">Energy consumption during July 2018</a>
+</li>
+<li>
+   <a href="powmon2019-08.php">Energy consumption during August 2018</a>
+</li>
+<li>
+   <a href="powmon2019-09.php">Energy consumption during September 2018</a>
+</li>
+<li>
+   <a href="powmon2019-11.php">Energy consumption during November 2018</a>
+</li>
+<li>
+   <a href="powmon2019-10.php">Energy consumption during October 2018</a>
+</li>
+<li>
+   <a href="powmon2019-12.php">Energy consumption during December 2018</a>
+</li>
+
+ </body>
+</html>
+
