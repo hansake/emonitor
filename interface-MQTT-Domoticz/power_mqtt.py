2047883b278c6d128ce8bca6620ef21ba75fbeb4
Hans-Ake <hansake@users.noreply.github.com>
2019-09-05 15:42:23 +0200

    Code is cleaned-up
    Domoticz device indexes updated to new installation.

--- interface-MQTT-Domoticz/power_mqtt.py
+++ interface-MQTT-Domoticz/power_mqtt.py
@@ -12,49 +12,66 @@
 import sqlite3 as sqlite, time, os
 import paho.mqtt.client as mqtt
 
+# Debug flag for printing, 0: false, 1: true
+Debug = 0
+
+# Configuration for MQTT broker and Domoticz
+broker_address="192.168.1.75"
+# The device indexes are assigned by Domoticz and must be changed here
+idx1 = 140
+idx2 = 141
+idx3 = 142
+
+# The Wh pulse counters
 pulsecounter1 = 0
 pulsecounter2 = 0
 pulsecounter3 = 0
 
 def GetCurrCounters(db_file):
-        global pulsecounter1
-        global pulsecounter2
-        global pulsecounter3
-        try:
-                dbconn=sqlite.connect(db_file)
-                dbdata=dbconn.cursor()
-                dbdata.execute("SELECT * FROM Lecounters")
-        except sqlite.Error,e:
-                print e
-        row = dbdata.fetchone()
-        pulsecounter1 = row[2]
-        pulsecounter2 = row[3]
-        pulsecounter3 = row[4]
-        if dbconn:
-                dbconn.close()
+    global pulsecounter1
+    global pulsecounter2
+    global pulsecounter3
+    try:
+        dbconn=sqlite.connect(db_file)
+        dbdata=dbconn.cursor()
+        dbdata.execute("SELECT * FROM Lecounters")
+    except sqlite.Error,e:
+        print e
+    row = dbdata.fetchone()
+    pulsecounter1 = row[2]
+    pulsecounter2 = row[3]
+    pulsecounter3 = row[4]
+    if dbconn:
+        dbconn.close()
 
 def main():
-    database = "/var/db//powermon.db"
+    database = "/var/db/powermon.db"
     GetCurrCounters(database)
-    print("Counter 1: {0} Wh, Counter 2: {1} Wh, Counter 3: {2} Wh".format(pulsecounter1, pulsecounter2, pulsecounter3))
 
-    broker_address="IP of my MQTT broker"
+    if Debug == 1:
+        print("Counter 1: {0} Wh, Counter 2: {1} Wh, Counter 3: {2} Wh".format(pulsecounter1, pulsecounter2, pulsecounter3))
+
     client = mqtt.Client("Ecnt1") #create new instance
     client.connect(broker_address) #connect to broker
 
-    idx1 = 322
+    # Format and publish first counter
     mqttbuffer = "{ \"idx\" : %d, \"nvalue\" : 0, \"svalue\" : \"%d\" }" % (idx1, pulsecounter1)
-    print(mqttbuffer)
-    client.publish("domoticz/in", mqttbuffer)#publish
+    if Debug == 1:
+        print(mqttbuffer)
+    client.publish("domoticz/in", mqttbuffer)
+    time.sleep(1) # wait a while between messages
 
-    idx2 = 323
+    # Format and publish second counter
     mqttbuffer = "{ \"idx\" : %d, \"nvalue\" : 0, \"svalue\" : \"%d\" }" % (idx2, pulsecounter2)
-    print(mqttbuffer)
+    if Debug == 1:
+        print(mqttbuffer)
     client.publish("domoticz/in", mqttbuffer)#publish
+    time.sleep(1) # wait a while between messages
 
-    idx3 = 324
+    # Format and publish third counter
     mqttbuffer = "{ \"idx\" : %d, \"nvalue\" : 0, \"svalue\" : \"%d\" }" % (idx3, pulsecounter3)
-    print(mqttbuffer)
+    if Debug == 1:
+        print(mqttbuffer)
     client.publish("domoticz/in", mqttbuffer)#publish
 
 if __name__ == '__main__':
